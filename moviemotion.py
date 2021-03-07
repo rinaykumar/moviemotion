48f02105ee800e4cbc778acd7d47a7d2edd0bdfa
rinaykumar <rkumar4@mail.sfsu.edu>
2021-03-07 06:59:59 -0800

    7am what we had

--- moviemotion.py
+++ moviemotion.py
@@ -2,6 +2,14 @@ API_TOKEN = ''
 
 import discord
 import text2emotion as te
+import random 
+
+# Movie arrays
+surprise = ["Behind Her Eyes", "The Sinner", "Tell Me Your Secrets", "The Blacklist", "Resident Alien", "I Care a Lot", "Mortal Kombat", "The Walking Dead", "Army of the Dead", "The Little Things", "WandaVision", "Superman and Lois", "The Mandalorian", "Spider-Man: No Way Home", "Tenet", "The Lord of the Rings: The Return of the King", "Inception", "Interstellar", "Seven Samurai", "The Lion King"]
+fear =["Attack on Titan", "Wrong Turn", "Supernatural", "American Horror Story", "The Vampire Diaries", "Saving Private Ryan", "The Pianist", "Casablanca", "Dara of Jasenovac", "Apocalypse Now", "Raatchasan", "The Silence of the Lambs", "Joker", "Oldboy", "Tumbbad", "WandaVision", "Supernatural", "Wonder Woman 1984", "The Suicide Squad", "Soul", "Shadow and Bone"]
+angry = ["The Matrix", "Star Wars: Episode V", "Gladiator", "Terminator 2", "Oldboy", "Aliens", "Warrior", "Die Hard", "Mission: Impossible", "Black Panther", "Snowpiercer", "Joker", "Pulp Fiction", "The Departed", "Goodfellas", "American Psycho", "Ocean’s Eleven", "Taxi Driver", "The Equalizer", "Scarface"]
+sad=["Hababam Sinifi","The Godfather","The Dark Knight","Schindler's List","Fight Club","Shadow of a Doubt","The Night of the Hunter","The Third Man","Touch of Evil","Out of the Past","Judas and the Black Messiah","It's a Sin","The Crown","The Trial of the Chicago 7","Age of Samurai: Battle for Japan","WandaVision“","Tribes of Europa","Game of Thrones","Outlander","Monster Hunter"]
+happy = ["Coming to America","Parasite","Once Upon a Time... In Hollywood","Thor: Ragnarok","The Gentlemen","Deadpool","Back to the Future","Snatch","The Sting","Lock, Stock and Two Smoking Barrels","The Grand Budapest Hotel","The Greatest Showman","Aladdin","The Sound of Music","The Blues Brothers","The Goonies","The Princess Bride","Wall-E","Toy Story","Friday"]
 
 client = discord.Client()
 
@@ -25,7 +33,7 @@ async def on_message(message):
                     # Save the messages to an array
                     messages = messages + " " + msg.content
 
-        # Remove newlines and tabs from messages
+        # Format messages
         messages = messages.replace('\n', '')
         messages = messages.replace('\t', '')
         messages = messages.strip('\n')  
@@ -40,13 +48,23 @@ async def on_message(message):
         print (emotions)
 
         # Result from text2emotion mapped to movie genres
+        Keymax = max(emotions, key=emotions.get) 
+        print(Keymax) 
 
         # Any movie db api call with selected genre
+        Start = 0
+        Stop = 19
+        limit = 3
+
+        RandomListOfIntegers = [random.randint(Start, Stop) for iter in range(limit)]
+
+        print(RandomListOfIntegers)
 
         # Process and format result from movie db api to list of 3-5 movies
 
         # DM the user                
         await message.author.send(f'You have **{counter}** messages')
         await message.author.send(f'{messages}')
+        await message.author.send('https://m.media-amazon.com/images/M/MV5BYzg0NGM2NjAtNmIxOC00MDJmLTg5ZmYtYzM0MTE4NWE2NzlhXkEyXkFqcGdeQXVyMTA4NjE0NjEy._V1_SX300.jpg')
 
 client.run(API_TOKEN)
